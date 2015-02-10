e6b739f9a17e68d567ce3d45bf0ee21f7ffa0495
Mark Jordan <mark.jordan@red-gate.com>
2015-02-10 19:35:39 +0000

    extract a class for making websockets nicer

--- scbot/slack/SlackRealTimeMessaging.cs
+++ scbot/slack/SlackRealTimeMessaging.cs
@@ -9,67 +9,27 @@ namespace scbot.slack
 {
     public class SlackRealTimeMessaging : IDisposable
     {
-        private readonly ClientWebSocket m_WebSocket;
+        private readonly StringClientWebSocket m_StringClientWebSocket;
 
-        private SlackRealTimeMessaging(ClientWebSocket webSocket)
+        private SlackRealTimeMessaging(StringClientWebSocket webSocket)
         {
-            m_WebSocket = webSocket;
+            m_StringClientWebSocket = webSocket;
         }
 
         public static async Task<SlackRealTimeMessaging> Connect(Uri wsUrl, CancellationToken cancellationToken)
         {
-            var webSocket = new ClientWebSocket();
-            await webSocket.ConnectAsync(wsUrl, cancellationToken);
-            return new SlackRealTimeMessaging(webSocket);
+            var ws = await StringClientWebSocket.Connect(wsUrl, cancellationToken);
+            return new SlackRealTimeMessaging(ws);
         }
 
         public async Task<string> Receive(CancellationToken cancellationToken)
         {
-            return await ReceiveString(cancellationToken);
-        }
-
-        private async Task<string> ReceiveString(CancellationToken cancellationToken)
-        {
-            // code based on https://msdn.microsoft.com/en-us/magazine/jj863133.aspx
-            var buffer = new byte[50*1024];
-            while (true)
-            {
-                var segment = new ArraySegment<byte>(buffer);
-                var result =
-                    await m_WebSocket.ReceiveAsync(segment, cancellationToken);
-                if (result.MessageType == WebSocketMessageType.Close)
-                {
-                    await m_WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "OK",
-                        cancellationToken);
-                    return null;
-                }
-                if (result.MessageType == WebSocketMessageType.Binary)
-                {
-                    await m_WebSocket.CloseAsync(WebSocketCloseStatus.InvalidMessageType,
-                        "I don't do binary", cancellationToken);
-                    return null;
-                }
-                int count = result.Count;
-                while (!result.EndOfMessage)
-                {
-                    if (count >= buffer.Length)
-                    {
-                        await m_WebSocket.CloseAsync(WebSocketCloseStatus.InvalidPayloadData,
-                            "That's too long", cancellationToken);
-                        return null;
-                    }
-                    segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
-                    result = await m_WebSocket.ReceiveAsync(segment, cancellationToken);
-                    count += result.Count;
-                }
-                var message = new UTF8Encoding(false).GetString(buffer, 0, count);
-                return message;
-            }
+            return await m_StringClientWebSocket.ReceiveString(cancellationToken);
         }
 
         public void Dispose()
         {
-            if (m_WebSocket != null) m_WebSocket.Dispose();
+            m_StringClientWebSocket.Dispose();
         }
     }
 }
---
+++ scbot/slack/StringClientWebSocket.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace scbot.slack
+{
+    public class StringClientWebSocket : IDisposable
+    {
+        private readonly ClientWebSocket m_WebSocket;
+
+        private StringClientWebSocket(ClientWebSocket webSocket)
+        {
+            m_WebSocket = webSocket;
+        }
+
+        public static async Task<StringClientWebSocket> Connect(Uri wsUrl, CancellationToken cancellationToken)
+        {
+            var webSocket = new ClientWebSocket();
+            await webSocket.ConnectAsync(wsUrl, cancellationToken);
+            return new StringClientWebSocket(webSocket);
+        }
+
+        public void Dispose()
+        {
+            if (m_WebSocket != null) m_WebSocket.Dispose();
+        }
+
+        public async Task<string> ReceiveString(CancellationToken cancellationToken)
+        {
+            // code based on https://msdn.microsoft.com/en-us/magazine/jj863133.aspx
+            var buffer = new byte[50*1024];
+            while (true)
+            {
+                var segment = new ArraySegment<byte>(buffer);
+                var result =
+                    await m_WebSocket.ReceiveAsync(segment, cancellationToken);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await m_WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "OK",
+                        cancellationToken);
+                    return null;
+                }
+                if (result.MessageType == WebSocketMessageType.Binary)
+                {
+                    await m_WebSocket.CloseAsync(WebSocketCloseStatus.InvalidMessageType,
+                        "I don't do binary", cancellationToken);
+                    return null;
+                }
+                int count = result.Count;
+                while (!result.EndOfMessage)
+                {
+                    if (count >= buffer.Length)
+                    {
+                        await m_WebSocket.CloseAsync(WebSocketCloseStatus.InvalidPayloadData,
+                            "That's too long", cancellationToken);
+                        return null;
+                    }
+                    segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
+                    result = await m_WebSocket.ReceiveAsync(segment, cancellationToken);
+                    count += result.Count;
+                }
+                var message = new UTF8Encoding(false).GetString(buffer, 0, count);
+                return message;
+            }
+        }
+    }
+}
