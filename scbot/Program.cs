c52668c9b1dcb7ca47fce48b7da739e9bec803c5
Mark Jordan <mark.jordan@red-gate.com>
2015-02-13 01:33:19 +0000

    hook up persistent persistence

--- scbot/Program.cs
+++ scbot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -16,7 +17,7 @@ namespace scbot
             var htmlDomainBlacklist = new[] {"jira", "jira.red-gate.com", "rg-jira01", "rg-jira01.red-gate.com", "redgatesupport.zendesk.com"};
             var processor = new ConcattingMessageProcessor(
                 new CompositeMessageProcessor(
-                    new NoteProcessor(new SlackCommandParser("scbot", "U03JWF43N"/*TODO*/), new NoteApi(new InMemoryKeyValueStore())),
+                    new NoteProcessor(new SlackCommandParser("scbot", "U03JWF43N"/*TODO*/), new NoteApi(new JsonFileKeyValueStore(new FileInfo("scbot.db.json")))),
                     new JiraBugProcessor(new JiraApi()),
                     new ZendeskTicketProcessor(ZendeskApi.Create(Configuration.RedgateId)),
                     new HtmlTitleProcessor(new HtmlTitleParser(), htmlDomainBlacklist)));
