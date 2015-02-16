dfde07d5ad401ea7990e85a9f84653e4d5bf28ec
Mark Jordan <mark.jordan@red-gate.com>
2015-02-16 12:52:29 +0000

    extract a couple useful variables

--- scbot/Program.cs
+++ scbot/Program.cs
@@ -15,9 +15,11 @@ namespace scbot
         public static void Main(string[] args)
         {
             var htmlDomainBlacklist = new[] {"jira", "jira.red-gate.com", "rg-jira01", "rg-jira01.red-gate.com", "redgatesupport.zendesk.com"};
+            var commandParser = new SlackCommandParser("scbot", "U03JWF43N"/*TODO*/);
+            var persistence = new JsonFileKeyValueStore(new FileInfo("scbot.db.json"));
             var processor = new ConcattingMessageProcessor(
                 new CompositeMessageProcessor(
-                    new NoteProcessor(new SlackCommandParser("scbot", "U03JWF43N"/*TODO*/), new NoteApi(new JsonFileKeyValueStore(new FileInfo("scbot.db.json")))),
+                    new NoteProcessor(commandParser, new NoteApi(persistence)),
                     new JiraBugProcessor(new JiraApi()),
                     new ZendeskTicketProcessor(ZendeskApi.Create(Configuration.RedgateId)),
                     new HtmlTitleProcessor(new HtmlTitleParser(), htmlDomainBlacklist)));
