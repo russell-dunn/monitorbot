4d86177cac4d3729341dd926938e27be3454b614
Mark Jordan <mark.jordan@red-gate.com>
2015-02-24 00:43:04 +0000

    first pass at making comparison generic

--- scbot/processors/teamcity/TeamcityBuildTracker.cs
+++ scbot/processors/teamcity/TeamcityBuildTracker.cs
@@ -18,15 +18,27 @@ namespace fasttests.teamcity
         private readonly ITeamcityBuildApi m_TeamcityBuildApi;
         private static readonly Regex s_TeamcityBuildRegex = new Regex(@"^build#(?<id>\d{5,10})$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private const string c_PersistenceKey = "tracked-tc-builds";
-        private readonly TeamcityBuildCompareEngine m_TeamcityBuildCompareEngine;
+        private readonly CompareEngine<TeamcityBuildStatus> m_TeamcityBuildCompareEngine;
 
         public TeamcityBuildTracker(scbot.slack.SlackCommandParser commandParser, scbot.services.InMemoryKeyValueStore persistence, ITeamcityBuildApi teamcityBuildApi)
         {
             m_CommandParser = commandParser;
             m_Persistence = new ListPersistenceApi<Tracked<TeamcityBuildStatus>>(persistence);
             m_TeamcityBuildApi = teamcityBuildApi;
-            m_TeamcityBuildCompareEngine = new TeamcityBuildCompareEngine(m_Persistence);
+            m_TeamcityBuildCompareEngine = new CompareEngine<TeamcityBuildStatus>(m_Persistence,
+                x => string.Format("<http://teamcity/viewLog.html?buildId={0}|Build {0}> ({1}) updated:", x.Id, x.Name),
+                new[] { new PropertyComparer<TeamcityBuildStatus>(x => x.OldValue.State != x.NewValue.State, x => FormatStateChanged(x))});
         }
+
+        private Response FormatStateChanged(Update<TeamcityBuildStatus> x)
+        {
+            if (x.NewValue.State == fasttests.teamcity.BuildState.Finished)
+            {
+                return new Response("build finished", null);
+            }
+
+            return new Response(string.Format("build state changed from {0} to {1}", x.OldValue.State, x.NewValue.State), null);
+        } 
 
         public MessageResult ProcessTimerTick()
         {
@@ -36,9 +48,16 @@ namespace fasttests.teamcity
                 new Update<TeamcityBuildStatus>(x.Channel, x.Value, m_TeamcityBuildApi.GetBuild(x.Value.Id).Result)
             ).Where(x => x.NewValue.IsNotDefault());
 
-            var responses = m_TeamcityBuildCompareEngine.CompareBuildStates(comparison);
+            var results = m_TeamcityBuildCompareEngine.CompareBuildStates(comparison).ToList();
 
-            return new MessageResult(responses.ToList());
+            foreach (var result in results)
+            {
+                var id = result.NewValue.Id;
+                m_Persistence.RemoveFromList(c_PersistenceKey, x => x.Value.Id == id);
+                m_Persistence.AddToList(c_PersistenceKey, new Tracked<TeamcityBuildStatus>(result.NewValue, result.Response.Channel));
+            }
+
+            return new MessageResult(results.Select(x => x.Response).ToList());
         }
 
         public MessageResult ProcessMessage(Message message)
---
+++ scbot/services/compareengine/CompareEngine.cs
@@ -0,0 +1,92 @@
+﻿using fasttests.teamcity;
+using scbot.services.compareengine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web.Helpers;
+
+namespace scbot.processors.teamcity
+{
+    class PropertyComparer<T> {
+        public readonly Func<Update<T>, bool> Compare;
+        public readonly Func<Update<T>, Response> Describe;
+
+        public PropertyComparer(Func<Update<T>, bool> comparer, Func<Update<T>, Response> describer)
+        {
+            Compare = comparer;
+            Describe = describer;
+        }
+
+        public static PropertyComparer<T> BasicEqualityComparer<TProp>(Expression<Func<Update<T>, TProp>> property, Func<TProp, TProp, string> describer)
+        {
+            return null; // TODO
+        }
+    }
+
+    class ComparisonResult<T>
+    {
+        public readonly Response Response;
+        public readonly T NewValue;
+
+        public ComparisonResult(Response response, T newValue)
+        {
+            Response = response;
+            NewValue = newValue;
+        }
+    }
+
+    class CompareEngine<T>
+    {
+        private services.ListPersistenceApi<Tracked<T>> m_Persistence;
+        private const string c_PersistenceKey = "tracked-tc-builds";
+        private readonly Func<T, string> m_PrefixStringGenerator;
+        private readonly List<PropertyComparer<T>> m_Comparers;
+
+        public CompareEngine(services.ListPersistenceApi<Tracked<T>> persistence, Func<T, string> prefixString, IEnumerable<PropertyComparer<T>> comparers)
+        {
+            m_Persistence = persistence;
+            m_PrefixStringGenerator = prefixString;
+            m_Comparers = new List<PropertyComparer<T>>(comparers);
+        }
+
+        internal IEnumerable<ComparisonResult<T>> CompareBuildStates(IEnumerable<Update<T>> comparison)
+        {
+            var differences = comparison.Select(x => new
+            {
+                differences = GetDifferenceString(x),
+                update = x
+            }).ToList();
+
+            differences = differences.Where(x => x.differences != null).ToList();
+
+            return differences.Select(x => new ComparisonResult<T>(
+                new Response(x.differences.Message, x.update.Channel, x.differences.Image), 
+                x.update.NewValue));
+        }
+
+        private Response GetDifferenceString(Update<T> ttc)
+        {
+            var differences = GetDifferences(ttc).ToList();
+            if (differences.Any())
+            {
+                var image = differences.Select(x => x.Image).FirstOrDefault(x => x.IsNotDefault());
+                return new Response(string.Format("{0} {1}",
+                    m_PrefixStringGenerator(ttc.NewValue), string.Join(", ", differences.Select(x => x.Message))), null, image);
+            }
+            return null;
+        }
+
+        private IEnumerable<Response> GetDifferences(Update<T> x)
+        {
+            foreach (var comparer in m_Comparers)
+            {
+                if (comparer.Compare(x))
+                {
+                    yield return comparer.Describe(x);
+                }
+            }
+        }
+    }
+}
