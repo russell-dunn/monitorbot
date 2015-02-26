2b543028ef9f2b71f7bb2890c8041ae873b1d53b
Mark Jordan <mark.jordan@red-gate.com>
2015-02-26 19:38:34 +0000

    add Branch class

---
+++ scbot.teamcity.webhooks/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scbot.teamcity.webhooks
+{
+    struct Branch
+    {
+        public readonly string Name;
+        public readonly string Repo;
+
+        public Branch(string name, string repo = null)
+        {
+            Name = name;
+            Repo = repo;
+        }
+    }
+}
