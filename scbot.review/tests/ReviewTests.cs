a7b270c3b6e213c0e863b6a85504a08b2db356b8
Mark Jordan <mark.jordan@red-gate.com>
2015-02-27 20:09:23 +0000

    add README to solution files

--- scbot.review/tests/ReviewTests.cs
+++ scbot.review/tests/ReviewTests.cs
@@ -20,7 +20,7 @@ namespace scbot.review.tests
             var reviewApi = new Mock<IReviewApi>();
             reviewApi.Setup(x => x.ReviewForPullRequest("fooCorp", "fooMatic", 123)).ReturnsAsync(
                 new[] { new DiffComment("this sucks", "f", 1), new DiffComment("this rocks", "g", 2) });
-            var commandParser = CommandlineParser.For("review #123");
+            var commandParser = CommandParser.For("review #123");
             var processor = new GithubReviewMessageProcessor(commandParser, reviewApi.Object, "fooCorp", "fooMatic");
 
             var response = processor.ProcessMessage(new Message("a-channel", "a-user", "msg"));
@@ -36,7 +36,7 @@ namespace scbot.review.tests
             reviewApi.Setup(x => x.ReviewForPullRequest("initech", "iniRepo", 123)).ReturnsAsync(
                 new[] { new DiffComment("this sucks", "f", 1), new DiffComment("this rocks", "g", 2) });
 
-            var commandParser = CommandlineParser.For("review initech/iniRepo#123");
+            var commandParser = CommandParser.For("review initech/iniRepo#123");
 
             var processor = new GithubReviewMessageProcessor(commandParser, reviewApi.Object, "fooCorp", "fooMatic");
 
