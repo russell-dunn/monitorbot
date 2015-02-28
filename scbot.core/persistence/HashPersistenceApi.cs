a73c45199a3f632fe8d9ae164a720086b3be0fef
Mark Jordan <mark.jordan@red-gate.com>
2015-02-28 13:44:17 +0000

    add a typed hash api
    This is still stupidly inefficient; we still don't care :3

---
+++ scbot.core/persistence/HashPersistenceApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+
+namespace scbot.core.persistence
+{
+    public class HashPersistenceApi<TValue> : IHashPersistenceApi<TValue>
+    {
+        private readonly IKeyValueStore m_Underlying;
+        private readonly string m_UnderlyingKey;
+
+        public HashPersistenceApi(IKeyValueStore store, string underlyingKey)
+        {
+            m_Underlying = store;
+            m_UnderlyingKey = underlyingKey;
+        }
+
+        private IDictionary<string, TValue> ReadAll()
+        {
+            return Json.Decode<IDictionary<string, TValue>>(m_Underlying.Get(m_UnderlyingKey));
+        }
+
+        public TValue Get(string key)
+        {
+            var dict = ReadAll();
+            if (dict.ContainsKey(key)) return dict[key];
+            return default(TValue);
+        }
+
+        public List<string> GetKeys()
+        {
+            return ReadAll().Keys.ToList();
+        }
+
+        public List<TValue> GetValues()
+        {
+            return ReadAll().Values.ToList();
+        }
+
+        public void Set(string key, TValue value)
+        {
+            var dict = ReadAll();
+            dict[key] = value;
+            m_Underlying.Set(m_UnderlyingKey, Json.Encode(dict));
+        }
+    }
+}
---
+++ scbot.core/persistence/IHashPersistenceApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scbot.core.persistence
+{
+    interface IHashPersistenceApi<TValue>
+    {
+        List<string> GetKeys();
+        List<TValue> GetValues();
+        TValue Get(string key);
+        void Set(string key, TValue value);
+    }
+}
