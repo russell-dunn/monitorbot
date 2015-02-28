aa545f2550bd76d39ef89c3604a310d309fb6066
Mark Jordan <mark.jordan@red-gate.com>
2015-02-28 15:12:13 +0000

    add new project for playing around with #14
    This includes a trivial git diff parser
    
    git diffs seem to have the nice property where you can almost parse the
    entire thing just by switching on the first character of each line

---
+++ scbot.review/diffparser/DiffLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scbot.review.diffparser
+{
+    public abstract class DiffLine
+    {
+        protected readonly string m_Line;
+        protected DiffLine(string line)
+        {
+            m_Line = line;
+        }
+
+        public override string ToString()
+        {
+            return "<" + this.GetType().Name + " " + m_Line + ">";
+        }
+    }
+
+    public class GitDiffHeader : DiffLine { public GitDiffHeader(string line) : base(line) { } }
+    public class ChunkHeader : DiffLine { public ChunkHeader(string line) : base(line.Substring(3)) { } }
+    public class OldFile : DiffLine { public OldFile(string line) : base(line.Substring(4)) { } }
+    public class NewFile : DiffLine { public NewFile(string line) : base(line.Substring(4)) { } }
+    public class ContextLine : DiffLine { public ContextLine(string line) : base(line.Substring(1)) { } }
+    public class AddedLine : DiffLine { public AddedLine(string line) : base(line.Substring(1)) { } }
+    public class RemovedLine : DiffLine { public RemovedLine(string line) : base(line.Substring(1)) { } }
+}
---
+++ scbot.review/tests/DiffParsingTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Net;
+using scbot.review.diffparser;
+
+namespace scbot.review.tests
+{
+    class DiffParsingTests
+    {
+        private void Log(string str) { Console.WriteLine(str); }
+
+        [Test]
+        public void CanParseDiff()
+        {
+            var client = new WebClient();
+            client.Encoding = new UTF8Encoding(false);
+            var diff = client.DownloadString("https://github.com/nyctef/sweetiebot/commit/6c27dccb66cb93654bbaf27573562ff715f42866.diff");
+            List<DiffLine> result = DiffParser.ParseDiff(diff);
+
+            foreach (var l in result) { Log(l.ToString()); }
+        }
+
+        
+    }
+}
