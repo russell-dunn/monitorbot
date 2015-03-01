06875be676c8509e38c705e7f7609d7cb075f46f
Mark Jordan <mark.jordan@red-gate.com>
2015-03-01 15:46:58 +0000

    remove Type from DiffComment since it's unused

--- scbot.review/reviewer/DiffComment.cs
+++ scbot.review/reviewer/DiffComment.cs
@@ -3,13 +3,11 @@
     public class DiffComment
     {
         public readonly string Description;
-        public readonly string Type;
         public readonly string File;
         public readonly int Line;
 
-        public DiffComment(string type, string description, string file, int line)
+        public DiffComment(string description, string file, int line)
         {
-            Type = type;
             Description = description;
             File = file;
             Line = line;
