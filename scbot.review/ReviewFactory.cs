aea55759dc3b779b9ef8e0a32adcf05358c63069
Mark Jordan <mark.jordan@red-gate.com>
2015-03-01 01:50:26 +0000

    add factory method for making review processor

---
+++ scbot.review/ReviewFactory.cs
@@ -0,0 +1,26 @@
+﻿using scbot.core.bot;
+using scbot.core.utils;
+using scbot.review.reviewer;
+using scbot.review.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scbot.review
+{
+	public static class ReviewFactory
+	{
+		public static IMessageProcessor GetProcessor(ICommandParser commandParser, IWebClient webClient, 
+			string githubToken, string defaultUser, string defaultRepo)
+		{
+			var githubApi = new GithubDiffApi(webClient, githubToken);
+			var reviewApi = new ReviewApi(githubApi, 
+				new Func<IDiffReviewer>[] {
+					() => new DontAddTabCharacters(),
+			});
+			return new GithubReviewMessageProcessor(commandParser, reviewApi, defaultUser, defaultRepo);
+		}
+	}
+}
