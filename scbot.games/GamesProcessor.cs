670c46f14e250ab3fd7c5105bdb06f15ffcd3f14
Mark Jordan <mark.jordan@red-gate.com>
2015-07-03 21:12:25 +0100

    first pass at tracking some games

---
+++ scbot.games/GamesProcessor.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using PlayerRank;
+using PlayerRank.Scoring;
+using scbot.core.bot;
+using scbot.core.persistence;
+using scbot.core.utils;
+
+namespace scbot.games
+{
+    public class GamesProcessor : ICommandProcessor
+    {
+        private readonly IKeyValueStore m_Persistence;
+        private readonly RegexCommandMessageProcessor m_Underlying;
+        private readonly EloScoringStrategy m_EloScoringStrategy = new EloScoringStrategy(maxRatingChange: 64, maxSkillGap: 400, startingRating: s_StartingRating);
+        private static readonly int s_StartingRating = 1000;
+
+        public GamesProcessor(IKeyValueStore persistence)
+        {
+            m_Persistence = persistence;
+            m_Underlying = new RegexCommandMessageProcessor(Commands);
+        }
+
+        public Dictionary<string, MessageHandler> Commands
+        {
+            get
+            {
+                return new Dictionary<string, MessageHandler>
+                {
+                    {@"record\s+(?<league>[^ ]+)\s+game\s*(?<results>.+)?", RecordGame}
+                };
+            }
+        }
+
+        private MessageResult RecordGame(Command command, Match args)
+        {
+            var responses = new List<Response>();
+            var league = args.Group("league");
+            var gamesPersistence = new ListPersistenceApi<Game>(m_Persistence, "games." + league);
+            var existingGames = gamesPersistence.ReadList();
+            var resultsString = args.Group("results");
+            var gameResults = ParseGameResults(resultsString);
+            var playersPersistence = new ListPersistenceApi<PlayerRating>(m_Persistence, "players." + league);
+            var players = playersPersistence.ReadList();
+            var playerNames = new HashSet<string>(players.Select(x => x.Name));
+            if (!existingGames.Any())
+            {
+                responses.Add(Response.ToMessage(command, string.Format("Creating new league `{0}`", league)));
+            }
+            if (!gameResults.Any())
+            {
+                if (String.IsNullOrWhiteSpace(resultsString))
+                {
+                    return Response.ToMessage(command, "Please provide some game results");
+                }
+                return Response.ToMessage(command, string.Format("Could not parse results `{0}`", resultsString));
+            }
+            var newGame = new Game(gameResults);
+            foreach (var result in gameResults)
+            {
+                if (!playerNames.Contains(result.Player))
+                {
+                    responses.Add(Response.ToMessage(command, string.Format("Adding new player `{0}`", result.Player)));
+                    playersPersistence.AddToList(new PlayerRating(result.Player, s_StartingRating));
+                }
+            }
+            var rankingChanges = CalculateRankingChanges(existingGames, newGame);
+            responses.AddRange(rankingChanges.Select(x => Response.ToMessage(command, x)));
+            gamesPersistence.AddToList(newGame);
+            return new MessageResult(responses);
+        }
+
+        private List<string> CalculateRankingChanges(List<Game> existingGames, Game newGame)
+        {
+            var league = new PlayerRank.League();
+            foreach (var existingGame in existingGames)
+            {
+                var leagueGame = GetPlayerRankGame(existingGame);
+                league.RecordGame(leagueGame);
+            }
+            var oldRankings = league.GetLeaderBoard(m_EloScoringStrategy);
+
+            league.RecordGame(GetPlayerRankGame(newGame));
+
+            var newRankings = league.GetLeaderBoard(m_EloScoringStrategy);
+
+            return CalculateRankingChanges(oldRankings.ToList(), newRankings.ToList(), newGame);
+        }
+
+        private List<string> CalculateRankingChanges(List<PlayerScore> oldRankings, List<PlayerScore> newRankings, Game newGame)
+        {
+            return newGame.Results.Select(
+                x => new {
+                        result = x,
+                        oldRating = GetRatingForPlayer(oldRankings, x.Player),
+                        newRating = GetRatingForPlayer(newRankings, x.Player)
+                    })
+                .Select(x => string.Format("{0}: **{1}** (new rating - {2})", x.result.Position, x.result.Player, x.newRating))
+                .ToList();
+        }
+
+        private double GetRatingForPlayer(List<PlayerScore> rankings, string player)
+        {
+            var ranking = rankings.FirstOrDefault(x => x.Name == player);
+            return ranking != null ? ranking.Score : s_StartingRating;
+        }
+
+        private static PlayerRank.Game GetPlayerRankGame(Game existingGame)
+        {
+            var leagueGame = new PlayerRank.Game();
+            foreach (var result in existingGame.Results)
+            {
+                // PlayerRank assumes that higher is better for scoring
+                leagueGame.AddResult(result.Player, 9999 - result.Position);
+            }
+            return leagueGame;
+        }
+
+        private List<PlayerPosition> ParseGameResults(string input)
+        {
+            var resultsRegex = new Regex(@"(?<resultString>(?<position>\d+)(st|nd|rd|th)\s+(?<player>[^\d]+))");
+            return resultsRegex.Matches(input)
+                .Cast<Match>()
+                .Select(result =>
+                {
+                    var player = result.Group("player").Trim();
+                    int position = Int32.Parse(result.Group("position"));
+                    return new PlayerPosition(player, position);
+                }).ToList();
+        }
+
+        public MessageResult ProcessTimerTick()
+        {
+            return m_Underlying.ProcessTimerTick();
+        }
+
+        public MessageResult ProcessCommand(Command command)
+        {
+            return m_Underlying.ProcessCommand(command);
+        }
+    }
+
+    internal struct PlayerRating
+    {
+        public readonly string Name;
+        public readonly int Rating;
+
+        public PlayerRating(string name, int rating)
+        {
+            Name = name;
+            Rating = rating;
+        }
+    }
+
+    internal struct PlayerPosition
+    {
+        public readonly string Player;
+        public readonly int Position;
+
+        public PlayerPosition(string player, int position)
+        {
+            Player = player;
+            Position = position;
+        }
+    }
+
+    internal struct Game
+    {
+        public readonly List<PlayerPosition> Results;
+
+        public Game(List<PlayerPosition> results)
+        {
+            Results = results;
+        }
+    }
+}
---
+++ scbot.games/tests/GamesTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using scbot.core.persistence;
+using scbot.core.tests;
+
+namespace scbot.games.tests
+{
+    class GamesTests
+    {
+        [Test]
+        public void CanRecordArbitraryGame()
+        {
+            var games = MakeGames();
+            var result = games.ProcessCommand("record worms game 1st Dave 2nd Pete 3rd Paul");
+            var expected = new[]
+            {
+                "Creating new league `worms`",
+                "Adding new player `Dave`",
+                "Adding new player `Pete`",
+                "Adding new player `Paul`",
+                "1: **Dave** (new rating - 1022)", 
+                "2: **Pete** (new rating - 1000)", 
+                "3: **Paul** (new rating - 978)",
+
+            };
+            var responses = result.Responses.Select(x => x.Message).ToList();
+            CollectionAssert.AreEqual(expected, responses);
+
+            result = games.ProcessCommand("record worms game 1st Dave 2nd Pete 3rd Paul");
+            expected = new[]
+            {
+                "1: **Dave** (new rating - 1041)", 
+                "2: **Pete** (new rating - 1000)", 
+                "3: **Paul** (new rating - 959)",
+            };
+            responses = result.Responses.Select(x => x.Message).ToList();
+            CollectionAssert.AreEqual(expected, responses);
+        }
+
+        [Test]
+        public void ComplainsAboutBadlyFormattedResults()
+        {
+            var games = MakeGames();
+            var result = games.ProcessCommand("record worms game 1x foo");
+            Assert.AreEqual("Could not parse results `1x foo`", result.Responses.Single().Message);
+        }
+
+        [Test]
+        public void ComplainsAboutMissingResults()
+        {
+            var games = MakeGames();
+            var result = games.ProcessCommand("record worms game");
+            Assert.AreEqual("Please provide some game results", result.Responses.Single().Message);
+        }
+
+        private GamesProcessor MakeGames()
+        {
+            return new GamesProcessor(new InMemoryKeyValueStore());
+        }
+    }
+}
