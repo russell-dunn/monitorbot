055ed74e6e76b0b397daaf733fc4f39d3041a614
David Eadie <david.eadie@red-gate.com>
2015-09-28 17:54:59 +0100

    Move away from old API
    The `int.Parse(newRanking.ToString())` is rather horrible but the new API doesn't currently have a way of getting the rating out as a double or int

--- scbot.games/GamesProcessor.cs
+++ scbot.games/GamesProcessor.cs
@@ -24,7 +24,7 @@ namespace scbot.games
 
         private readonly IKeyValueStore m_Persistence;
         private readonly RegexCommandMessageProcessor m_Underlying;
-        private readonly EloScoringStrategy m_EloScoringStrategy = new EloScoringStrategy(maxRatingChange: 64, maxSkillGap: 400, startingRating: s_StartingRating);
+        private readonly EloScoringStrategy m_EloScoringStrategy = new EloScoringStrategy(maxRatingChange: new Points(64), maxSkillGap: new Points(400), startingRating: new Points(s_StartingRating));
         private static readonly int s_StartingRating = 1000;
 
         public GamesProcessor(IKeyValueStore persistence)
@@ -86,7 +86,7 @@ namespace scbot.games
             foreach (var player in newGame.Results.Select(x => x.Player))
             {
                 var newRanking = GetRatingForPlayer(newLeaderboard.ToList(), player);
-                playersPersistence.Set(player, (int)newRanking);
+                playersPersistence.Set(player, int.Parse(newRanking.ToString()));
             }
 
             var rankingChanges = GetResultsWithRankingChanges(oldLeaderboard.ToList(), newLeaderboard.ToList(), newGame);
@@ -165,10 +165,10 @@ namespace scbot.games
                 .ToList();
         }
 
-        private double GetRatingForPlayer(List<PlayerScore> rankings, string player)
+        private Points GetRatingForPlayer(List<PlayerScore> rankings, string player)
         {
             var ranking = rankings.FirstOrDefault(x => x.Name == player);
-            return ranking != null ? ranking.Score : s_StartingRating;
+            return ranking != null ? ranking.Points : new Points(s_StartingRating);
         }
 
         private static PlayerRank.Game GetPlayerRankGame(Game existingGame)
@@ -177,7 +177,7 @@ namespace scbot.games
             foreach (var result in existingGame.Results)
             {
                 // PlayerRank assumes that higher is better for scoring
-                leagueGame.AddResult(result.Player, 9999 - result.Position);
+                leagueGame.AddResult(result.Player, new Position(result.Position));
             }
             return leagueGame;
         }
