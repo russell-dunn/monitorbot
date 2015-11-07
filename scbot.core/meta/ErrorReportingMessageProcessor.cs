6c4c63b79cc43bfc1b1f8125ce457dd9f95fabf0
Nyctef <nyctef@nyctef.com>
2015-11-07 14:37:38 +0000

    exception handling must never throw an exception

--- scbot.core/meta/ErrorReportingMessageProcessor.cs
+++ scbot.core/meta/ErrorReportingMessageProcessor.cs
@@ -46,17 +46,24 @@ namespace scbot.core.meta
 
         private MessageResult LogException(Exception exception, Message? incomingMessage = null)
         {
-            var message = incomingMessage != null ? 
-                incomingMessage.Value.Channel + " " + incomingMessage.Value.User + " " + incomingMessage.Value.MessageText :
-                "tick";
-            var report = string.Format("{0}: {1}\n{2}", DateTime.Now, message, exception.ToString());
-            var pasteUrl = m_Pastebin.UploadPaste(report);
-            var response = string.Format("DANGER WILL ROBINSON: A <{0}|{1}> was encountered while processing this request.",
-                pasteUrl, exception.GetType().Name);
-            Trace.TraceError(response + "\n" + exception.ToString());
-            if (incomingMessage != null)
+            try
             {
-                return Response.ToMessage(incomingMessage.Value, response);
+                var message = incomingMessage != null ?
+                    incomingMessage.Value.Channel + " " + incomingMessage.Value.User + " " + incomingMessage.Value.MessageText :
+                    "tick";
+                var report = string.Format("{0}: {1}\n{2}", DateTime.Now, message, exception.ToString());
+                var pasteUrl = m_Pastebin.UploadPaste(report);
+                var response = string.Format("DANGER WILL ROBINSON: A <{0}|{1}> was encountered while processing this request.",
+                    pasteUrl, exception.GetType().Name);
+                Trace.TraceError(response + "\n" + exception.ToString());
+                if (incomingMessage != null)
+                {
+                    return Response.ToMessage(incomingMessage.Value, response);
+                }
+            }
+            catch (Exception anotherException)
+            {
+                Trace.TraceError("Exception while handling exception:\n" + anotherException);
             }
             return MessageResult.Empty;
         }
