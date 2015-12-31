e8fd8aef7fc7d1e5d82a4f2442fa448f648fe443
Nyctef <nyctef@nyctef.com>
2015-12-31 20:15:29 +0000

    add IAliasList concept

---
+++ scbot.core/utils/NullAliasList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scbot.core.utils
+{
+    public class NullAliasList : IAliasList
+    {
+        public string GetCanonicalNameFor(string name)
+        {
+            return name;
+        }
+
+        public string GetDisplayNameFor(string name)
+        {
+            return name;
+        }
+    }
+}
