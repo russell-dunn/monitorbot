13699b7637412846888a7c8d1e8b4ebe4ad0b57b
Nyctef <nyctef@nyctef.com>
2015-12-31 21:25:27 +0000

    implement display names with a bunch of different test cases

--- scbot.core/tests/AliasListTests.cs
+++ scbot.core/tests/AliasListTests.cs
@@ -14,9 +14,20 @@ namespace scbot.core.tests
         public void CanAddAndFetchDisplayName()
         {
             var subject = new AliasList();
-            subject.AddAlias("hiro.protagonist", "Hiro Protagonist", new[] { "The Deliverator", "GreatestSwordsman " });
+            subject.AddAlias("hiro.protagonist", "Hiro Protagonist", new[] { "The Deliverator", "GreatestSwordsman" });
 
-            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("Hiro Protagonist"));
+            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("Hiro Protagonist"), 
+                "should pass through display name");
+            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("hiro.protagonist"),
+                "should convert canonical name to display name");
+            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("The Deliverator"),
+                "other aliases should also work");
+            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("GreatestSwordsman"),
+                "more than one other aliases should also work");
+            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("HIRO.PROTAGONIST"),
+                "should not be case-sensitive");
+            Assert.AreEqual("Hiro Protagonist", subject.GetDisplayNameFor("HIRO PROTAGONIST"), 
+                "should canonicalize display name");
         }
     }
 }
--- scbot.core/utils/AliasList.cs
+++ scbot.core/utils/AliasList.cs
@@ -8,6 +8,9 @@ namespace scbot.core.utils
 {
     public class AliasList : IAliasList
     {
+        private readonly Dictionary<string, string> m_NamesToDisplayName
+            = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
         public string GetCanonicalNameFor(string name)
         {
             return name;
@@ -15,11 +18,22 @@ namespace scbot.core.utils
 
         public string GetDisplayNameFor(string name)
         {
+            string displayName;
+            if (m_NamesToDisplayName.TryGetValue(name, out displayName))
+            {
+                return displayName;
+            }
             return name;
         }
 
         public void AddAlias(string canonicalName, string displayName, IEnumerable<string> otherAliases)
         {
+            m_NamesToDisplayName.Add(canonicalName, displayName);
+            m_NamesToDisplayName.Add(displayName, displayName);
+            foreach (var otherAlias in otherAliases)
+            {
+                m_NamesToDisplayName.Add(otherAlias, displayName);
+            }
         }
     }
 }
